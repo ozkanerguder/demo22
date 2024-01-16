2883b97d6b2f86622032a2c7d2316ab579b2ed44
Özkan Ergüder <erguderozkan@gmail.com>
2024-01-16 11:24:26 +0300

    max length 70

--- management/models.py
+++ management/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class xreservation( models.Model ):
     Client = models.CharField(
-        max_length=60)
+        max_length=70)
     website = models.URLField( )
     email = models.EmailField( )
