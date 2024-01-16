3be211882f33ccf402192fe5e51a473018381d85
Özkan Ergüder <erguderozkan@gmail.com>
2024-01-16 11:30:11 +0300

    models changed

--- management/models.py
+++ management/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class xreservation( models.Model ):
     Client = models.CharField(
-        max_length=50)
+        max_length=60)
     website = models.URLField( )
     email = models.EmailField( )
